6d147a39519164ee8ac1074b60425e964e9ab996
FritzAmpo <157578809+FritzAmpo@users.noreply.github.com>
2024-01-24 17:29:45 +0800

    Add files via upload

---
+++ Activities/Activity I/AMPO_ACTIVITY_I.java
@@ -0,0 +1,26 @@
+public class AMPO_ACTIVITY_I {    
+public static void main(String[] args) {
+    //declare variables
+    int a=12;
+    int b=8;
+    int c=5;
+
+    // Addition
+    int add;
+        add= c+b;
+    System.out.println(x:"a+b=");
+    //Subtraction
+    int Subtraction;
+        Sub= a-b;
+    System.out.println(x:"a-b="+(a-b));
+    //
+}
+
+
+
+
+
+
+
+
+}
