04507cb66776abc857e662f7a75c019b33a38a21
tommysumonod <157578822+tommysumonod@users.noreply.github.com>
2024-01-24 17:30:11 +0800

    Add files via upload

---
+++ Activities/Activity I/SUMONOD_ACTIVITY_I.java
@@ -0,0 +1,14 @@
+public class SUMONOD_ACTIVITY_I{
+  public static void main (String []args){
+    int num1 = 4;
+    int num2 = 2;
+    int num3 = 1;
+
+    System.out.println(num1 / 3);
+    System.out.println(num1 + 3);
+    System.out.println(num1 - 3);
+    System.out.println(num3 * num3);
+
+    System.out.println("Result 1 = 1.0");
+  }
+}
