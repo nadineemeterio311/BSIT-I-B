344a9d7f9518156d6bd1f2f9e376d68277d28755
C. Adrian Jay <117725727+K1gwa@users.noreply.github.com>
2024-01-24 17:30:57 +0800

    Add files via upload

---
+++ CAMPO.java
@@ -0,0 +1,18 @@
+public class CAMPO_ACTIVITY_I { 
+    public static void main(String[] args) {
+   
+   int A =2;
+   int B =30;
+   int C =20;
+
+   int SUM= A+C; 
+   int DIFFERENCE= B-A; 
+   int PRODUCT= B*A; 
+   int QUOTIENT= A/C; 
+   
+   system.out.println(SUM);
+   system.out.println(DIFFERENCE);
+   system.out.println(PRODUCT);
+   system.out.println(QUOTIENT);
+    }
+   }
