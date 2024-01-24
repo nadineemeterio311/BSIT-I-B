3f937bdefdb9ef711cc48b182635bb5ea6a0f30e
C. Adrian Jay <117725727+K1gwa@users.noreply.github.com>
2024-01-24 18:06:52 +0800

    Update and rename CAMPO.java to CAMPO_ACTIVITY_I.java

---
+++ CAMPO_ACTIVITY_I.java
@@ -0,0 +1,18 @@
+public class CAMPO_ACTIVITY_I {
+    public static void main(String[] args) {
+
+   int A =2;
+   int B =30;
+   int C =20;
+
+   int SUM= A+C;
+   int DIFFERENCE= B-A;
+   int PRODUCT= B*A;
+   int QUOTIENT= C/A;
+
+   System.out.println(SUM);
+   System.out.println(DIFFERENCE);
+   System.out.println(PRODUCT);
+   System.out.println(QUOTIENT);
+    }
+   }
