db3cc3f6e4e3ba1c3ba1d52909ed64e8ddd21b45
RayLeonardPescuela <157578744+RayLeonardPescuela@users.noreply.github.com>
2024-01-24 17:30:55 +0800

    Add files via upload

---
+++ Activities/Activity I/PESCUELA_ACTIVITY_I.java
@@ -0,0 +1,20 @@
+public class PESCUELA_ACTIVITY_I{
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 10;
+        int num3 = 15;
+
+        int sum = num1 + num2 + num3;
+        int diff = num1 - num2 - num3;
+        int prod = num1 * num2 * num3;
+        int quot = num1 / num2 / num3;
+
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(prod);
+        System.out.println(qout);
+
+
+
+    }
+}
