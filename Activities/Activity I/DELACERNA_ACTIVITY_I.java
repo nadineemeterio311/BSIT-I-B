c49c22c41b65f799ed5c778c847b34a51e9a1b40
Jan Khalil C. Dela Cerna <153181702+jankhalil1@users.noreply.github.com>
2024-01-24 17:27:38 +0800

    Add files via upload

---
+++ Activities/Activity I/DELACERNA_ACTIVITY_I.java
@@ -0,0 +1,18 @@
+public class DELACERNA_ACTIVITY_I {
+    public static void main(String[]args){
+        int a = 25;
+        int b = 50;
+        int c = 75;
+
+        int sum = a + b;
+        int difference = c - a;
+        int product = a * b;
+        int quotient = c / a; 
+
+        System.out.println(sum);
+        System.out.println(difference);
+        System.out.println(product);
+        System.out.println(quotient);
+ 
+}   
+}
