f2b9c9a06e0bcaa43d325156631d6b8754112f66
BlessAntemaro <157581023+BlessAntemaro@users.noreply.github.com>
2024-01-24 17:32:34 +0800

    Add files via upload

---
+++ Activities/Activity I/ANTEMARO_ACTIVITY_I.java
@@ -0,0 +1,14 @@
+public class ANTEMARO_ACTIVITY_I{
+    public static void main (String[]args){
+        int x = 30;
+        int y = 20;
+        int z = 50;
+        System.out.println(x*y/z);
+        System.out.println(x+y+z);
+        System.out.println(x-y-z);
+        System.out.println(x*y*z);
+        System.out.println(x/y/z);
+     }
+}
+
+     
