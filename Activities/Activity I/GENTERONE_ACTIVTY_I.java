9e7953eb58a8cd0f2921077e2e837b9d2e6c7674
johnlhester <157578893+johnlhester@users.noreply.github.com>
2024-01-24 17:31:24 +0800

    Add files via upload

---
+++ Activities/Activity I/GENTERONE_ACTIVTY_I.java
@@ -0,0 +1,22 @@
+public class GENTERONE_ACTIVTY_I {
+
+public static void main(String[] args); {
+
+    Int x= 5;
+    Int y= 5;
+    Int z= 10;
+    Int c= 20;
+
+    int sum = x+y;
+    int difference = x-z;
+    int product = x*z;
+    int qoutient = z/c;
+
+   System.out.println(sum);
+   System.out.println(difference);
+   System.out.println(product);
+   System.out.println(qoutient);
+
+    }
+
+}
