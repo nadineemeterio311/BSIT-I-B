e73a435b48fedd20e8532b70a44abdf987ab1497
RogelioAntipuestoJr <157578821+RogelioAntipuestoJr@users.noreply.github.com>
2024-01-24 17:32:48 +0800

    Add files via upload

---
+++ Activities/Activity I/Antipuesto_Activity_1.java
@@ -0,0 +1,26 @@
+public class Antipuesto_Activty_1 {
+
+    public static void main {String[] args} {
+
+        int x = 15;
+        int y = 10;
+        int z = 5;
+
+        int sum = x + y;
+        int difference = x - y;
+        int product = x * z;
+        int qoutient = z / x;
+
+
+        System.out.println(sum);
+        System.out.println(difference)
+        System.out.println(product)
+        System.out.println(qoutient)
+
+        
+    
+    }
+)
+ 
+  
+
