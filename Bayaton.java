688bafd11a12f00c7159d351a08d01451a94beb0
BayatonFrankie <156895146+BayatonFrankie@users.noreply.github.com>
2024-01-24 17:33:55 +0800

    Add files via upload

---
+++ Bayaton.java
@@ -0,0 +1,17 @@
+public class bayaton_activity_1{
+    public static void main(String[]args){
+        int a = 3;
+        int b = 2;
+        int c = 4;
+
+        int d = a+b;
+        int e = a-c;
+        int f = c*a;
+        int g = a/b;
+
+        System.out.println ("Addition:" + d);
+        System.out.println ("Subtraction:" - e);
+        System.out.println ("Multiply:" * f);
+        System.out.println ("Divide:" / g);
+    }
+}   
