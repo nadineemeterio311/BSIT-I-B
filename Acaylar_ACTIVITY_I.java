ca9a3674bbf1938bad9af56ec77b9ac8e6a0c24c
CareyAcaylar <157579052+CareyAcaylar@users.noreply.github.com>
2024-01-24 17:33:10 +0800

    Add files via upload

---
+++ Acaylar_ACTIVITY_I.java
@@ -0,0 +1,24 @@
+public class Acaylar_ACTIVITY_I {
+public static void main(String[] args) {
+    //Declare Variables
+    int a = 12;
+    int b = 7;
+    int c = 8;
+
+    int sum = c + b;
+    int difference = a - b;
+    int product = a * b;
+    int quotient = a / c;
+    
+    System.out.println(sum);
+    System.out.println(difference);
+    System.out.println(product);
+    System.out.println(quotient);
+
+}
+
+
+
+
+
+}
