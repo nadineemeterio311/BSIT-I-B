1e7316f434845760438aa8ef6384838b9f56d94b
Boyles, Prince Niño L <94779042+JesseMc87@users.noreply.github.com>
2024-01-24 17:27:49 +0800

    Add files via upload
    Activity-1

---
+++ Activities/Activity I/BOYLES_ACTIVITY_I.java
@@ -0,0 +1,20 @@
+public class BOYLES_ACTIVITY_I {
+    public static void main(String[] args){
+        int a = 129;
+        int y = 280;
+        int x = 300;
+        int z = a+y;
+        int s = x-a;
+        int c = x/a;
+        int w = y*x;
+        
+        System.out.println("Quotient = " + w );
+        System.out.println("Sum = " + z);
+        System.out.println("Difference = " + s);
+        System.out.println("Product = " + c);
+    
+        
+     
+    }
+
+}
