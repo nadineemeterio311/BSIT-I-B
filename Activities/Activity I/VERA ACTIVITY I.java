52573b0c3987fca053f9e752762f8ee9b93ad58d
francisvera <157578852+francisvera@users.noreply.github.com>
2024-01-24 17:31:58 +0800

    Add files via upload

---
+++ Activities/Activity I/VERA ACTIVITY I.java
@@ -0,0 +1,29 @@
+public class VERA ACTIVITY I{
+
+
+
+public static void main(String[]args){
+
+  int x= 5;
+  int y= 4;
+  int z= 3;
+   
+  int sum=x+y;
+  int difference= y-z;
+  int product=x*z;
+  int quotient=z/x;
+
+
+
+
+  System.out.println(sum);
+  System.out.println(difference);
+  System.out.println(product);
+  System.out.println(quotient);
+  
+
+
+
+  }
+}
+
