cb344b649e95f410d3758fb44429d432d4f66f79
Clint Wilson N. Gonzales <157579219+clintwilson123@users.noreply.github.com>
2024-01-24 17:29:47 +0800

    Add files via upload

---
+++ Activities/Activity I/[Gonzales_ACTIVITY_I].java
@@ -0,0 +1,36 @@
+public class Gonzales_ACTIVITY_I {
+    public static void main (String[]args) {
+        int a = 8;
+        int b = 5;
+     
+        
+        //Addition
+        int sum = a + b;
+        //Subtraction
+        int diffirence = a - b;
+        //Multiplication
+        int product = a * b;
+        //Division
+        int quotient = a / b;
+        //Modulo
+        int modulo = a % b;
+
+
+
+         
+         
+
+
+        
+        System.out.println("Additon: " + sum);
+        System.out.println("Subtraction: " + diffirence);
+        System.out.println("Multiplication: " + product);
+        System.out.println("Division:  " + quotient );
+        System.out.println("Remainder: " + modulo);
+
+
+
+
+    }
+    
+}
