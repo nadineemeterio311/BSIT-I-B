98d28184c5b615475b188201c37cf8bcb6fca091
clancedavesarsalejo <157579179+clancedavesarsalejo@users.noreply.github.com>
2024-01-24 17:33:25 +0800

    Add files via upload

---
+++ Sarsalejo.java
@@ -0,0 +1,19 @@
+public class Sarsalejo_Activity_I {
+    pulic static void main (String []args){
+
+        int a = 15;
+        int b = 30;
+        int c = 45;
+
+        int d = a+b;
+        int e = a-c;
+        int f = c*a;
+        int g = b/a;
+
+        System.out.println("Addition = " + d);
+        System.out.println("Subtraction = " + e);
+        System.out.println("Multiply = " + f);
+        System.out.println("Divide =" + g);
+
+    }
+}
