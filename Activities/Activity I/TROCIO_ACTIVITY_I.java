892be5dbe28ef424d50488cd7881dae28175d115
Jushua Trocio <156936077+Jushua0909@users.noreply.github.com>
2024-01-24 17:29:06 +0800

    Add files via upload

---
+++ Activities/Activity I/TROCIO_ACTIVITY_I.java
@@ -0,0 +1,18 @@
+public class TROCIO_ACTIVITY_I {
+    public  static void main (String []args) {
+int a = 1;
+int b = 23;
+int c = 2004;
+
+int d = a+b;
+int e = a+c;
+int f = c*a;
+int g = a/b;
+
+System.out.println("Addition:" + d);
+        System.out.println("Multiplication:" + e);
+        System.out.println("Subtraction:" + f);
+        System.out.println("Division:" + g);
+    }
+
+}
