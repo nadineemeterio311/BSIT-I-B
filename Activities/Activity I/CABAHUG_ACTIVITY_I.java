7ccfdeb3949e04a32ba77ab3848c9790f5e4bf53
cabahugrechelle <157580783+cabahugrechelle@users.noreply.github.com>
2024-01-24 17:30:44 +0800

    Add files via upload

---
+++ Activities/Activity I/CABAHUG_ACTIVITY_I.java
@@ -0,0 +1,13 @@
+public class CABAHUG_ACTIVITY_I{
+    public static void main(String[]args){
+        int x = 30;
+        int y = 20;
+        int z = 50;
+        System.out.println(x*y/z);
+        System.out.println(x+y+z);
+        System.out.println(x-y-z);
+        System.out.println(x*y*z);
+        System.out.println(x/y/z);
+
+    }
+}
