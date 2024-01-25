0f494bf218b03bfa8df845434541df18c95a8f92
anjenneth <157085781+eijeyy0@users.noreply.github.com>
2024-01-25 01:43:54 -0800

    Update Alcontin.java

---
+++ Activities/Activity I/Alcontin.java
@@ -0,0 +1,17 @@
+public class ALCONTIN_ACTIVITY_I {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        int c = 30;
+
+        int sum = a + b;
+        int difference = b - a;
+        int product = b * c;
+        int quotient = c / a;
+
+        System.out.println(sum);
+        System.out.println(difference);
+        System.out.println(product);
+        System.out.println(quotient);
+    }
+}
