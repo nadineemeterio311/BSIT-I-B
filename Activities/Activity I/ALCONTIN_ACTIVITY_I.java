677e5124863e715ce55797816ecb69c07a6057b5
anjenneth <157085781+eijeyy0@users.noreply.github.com>
2024-01-24 19:36:17 -0800

    Update ALCONTIN_ACTIVITY_I.java

---
+++ Activities/Activity I/ALCONTIN_ACTIVITY_I.java
@@ -0,0 +1,13 @@
+public class LAPINCAO_ACTIVITY_I{
+    public static void main(String[]args){
+        int x = 10;
+        int y = 20;
+        int z = 15;
+
+        System.out.println(x*y-z);
+        System.out.println(x+y+z);
+        System.out.println(x-y-z);
+        System.out.println(x*y*z);
+        System.out.println(x/y/z);
+    }
+}
