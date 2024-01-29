2f6b4dd1a8492f958ce8a04d6f6317454576f19c
juliefel <157578767+juliefel12@users.noreply.github.com>
2024-01-29 13:08:58 +0800

    Add files via upload

---
+++ MALUSAY_)ACTIVITY_1.java
@@ -0,0 +1,20 @@
+package MALUSAY_ACTIVITY_1.java;
+
+public class MALUSAY_)ACTIVITY_1 {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        int c = 30;
+
+        int sum = a + b;
+        int difference = b - a;
+        int product = b * c;
+        int quotient = c / a;
+
+        System.out.println(sum);
+        System.out.println(difference);
+        System.out.println(product);
+        System.out.println(quotient);
+    }
+
+}
