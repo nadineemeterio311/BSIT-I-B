b0926ff6bdcd475fe6a04737d0a0dfb09a7b1266
EnojadaR <158020976+EnojadaR@users.noreply.github.com>
2024-01-29 14:23:38 +0800

    Add files via upload

---
+++ ENOJADA_ACTIVITY_I.java
@@ -0,0 +1,15 @@
+public class ENOJADA_ACTIVITY_I.java {
+
+    public static void main (String{}args ){
+
+        int x = 30;
+        int y = 20;
+        int z = 60;
+
+        System.out.println(x*y*z);
+        System.out.println(x+y+z);
+        System.out.println(x-y-z);
+        System.out.println(x/y/z);
+
+    }
+}
