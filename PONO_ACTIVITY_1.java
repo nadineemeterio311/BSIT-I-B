73461ad0e0629162e1fdcfae9e0bf9a3639506a0
yvonne pono <157579481+yv0nne1@users.noreply.github.com>
2024-01-29 12:45:42 +0800

    Add files via upload

---
+++ PONO_ACTIVITY_1.java
@@ -0,0 +1,21 @@
+public class PONO_ACTIVITY_1 { 
+    public static void main(String[] args) {
+        int a = 50;
+        int b = 20;
+        int c = 40;
+
+        int d = a+b;
+        int e = a+c;
+        int f = c*a;
+        int g = a/b;
+
+        System.out.println("Addition:" + d);
+        System.out.println("Multiplication:" + e);
+        System.out.println("Subtraction:" + f);
+        System.out.println("Division:" + g);
+    }
+
+}
+
+    
+
